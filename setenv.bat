ec141ff9c3704e7572bc1bdedd06e2504ea7758c
mysonnet <mysonnet+github@gmail.com>
2014-01-19 06:01:36 +0900

    setenv add

---
+++ setenv.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+SET PATH_ROOT=%~dp0
+IF %PROCESSOR_ARCHITECTURE% EQU x86 SET PATH=%PATH%;%PATH_ROOT%App\redisbin
+IF %PROCESSOR_ARCHITECTURE% NEQ x86 SET PATH=%PATH%;%PATH_ROOT%App\redisbin64
+
+start redis-server.exe
+start redis-cli.exe
+
+
+endlocal
